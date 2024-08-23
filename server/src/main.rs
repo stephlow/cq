a4fbd6e5f2a61f4a30b2e8acec94eeeb7217061b
Stephan Lagerwaard <stephanlagerwaard@gmail.com>
2024-08-23 19:45:07 +0200

    App setup

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,15 @@
+use bevy::{
+    app::{App, PluginGroup, ScheduleRunnerPlugin},
+    MinimalPlugins,
+};
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(
+            MinimalPlugins.set(ScheduleRunnerPlugin::run_loop(Duration::from_secs_f64(
+                1.0 / 60.0,
+            ))),
+        )
+        .run();
 }
