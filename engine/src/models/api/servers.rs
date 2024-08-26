8ccdef5031de045f7421ef2f4f9548c3d74f1dae
Stephan Lagerwaard <stephanlagerwaard@gmail.com>
2024-08-26 19:37:30 +0200

    Cleanup

--- engine/src/models/api/servers.rs
+++ engine/src/models/api/servers.rs
@@ -1,9 +1,8 @@
+use crate::models;
 use serde::{Deserialize, Serialize};
 use std::net::IpAddr;
 use time::OffsetDateTime;
 use uuid::Uuid;
-
-use crate::models;
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Server {
@@ -33,7 +32,6 @@ impl Server {
 
 impl From<models::data::servers::Server> for Server {
     fn from(value: models::data::servers::Server) -> Self {
-        // let addr = IpAddr::from_str(&value.addr).expect("Invalid ip");
         let port: u16 = value.port.try_into().expect("Invalid port");
 
         Self {
